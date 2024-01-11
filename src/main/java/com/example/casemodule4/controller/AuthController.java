b577aacf159e337def62971c820b0aa7ad2f0291
buianh256 <ducanhlccd1@example.com>
2024-01-11 14:49:16 +0700

    update AuthController in controller

--- src/main/java/com/example/casemodule4/controller/AuthController.java
+++ src/main/java/com/example/casemodule4/controller/AuthController.java
@@ -1,5 +1,40 @@
 package com.example.casemodule4.controller;
 
+import com.example.casemodule4.model.Users;
+import com.example.casemodule4.security.service.JwtResponse;
+import com.example.casemodule4.security.service.JwtService;
+import com.example.casemodule4.security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Users user) {
+        Authentication authentication
+                = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtService.generateTokenLogin(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Users currentUser = userService.findByUsername(user.getUsername());
+        return ResponseEntity.ok(new JwtResponse(currentUser.getId(), jwt, userDetails.getUsername(), userDetails.getUsername(), userDetails.getAuthorities()));
+    }
 }
