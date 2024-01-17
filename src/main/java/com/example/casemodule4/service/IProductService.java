72caeaa56dbd4137547a33adf826ff8509833e64
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2024-01-17 09:17:57 +0700

    category

--- src/main/java/com/example/casemodule4/service/IProductService.java
+++ src/main/java/com/example/casemodule4/service/IProductService.java
@@ -4,19 +4,5 @@ import com.example.casemodule4.model.Product;
 import com.example.casemodule4.model.SearchOBJ;
 
 public interface IProductService extends IGenerateService<Product> {
-    Iterable<Product> findAll ( );
-
-    void reduceQuantity(Long id, int quantity);
-
-    void reducePrice(Long id, double price);
-
-    Iterable<Product> searchByGender(String gender);
-
-    Iterable<Product> searchProductByName(String name);
-
-    Iterable<Product> filter(SearchOBJ searchOBJ);
-
-//    Iterable<OrderProduct> reportByYear(SearchOBJ searchOBJ);
-//    Iterable<OrderProduct> reportByMonth(SearchOBJ searchOBJ);
 
 }
